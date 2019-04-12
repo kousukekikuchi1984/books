b4020c8bc055b375b4f5835d4962f5d04e591b01
kousukekikuchi1984 <kousukekikuchi1984@gmail.com>
2019-04-12 10:07:50 +0900

    added 1.1.2

--- open_data_structures/1-introduction/1.1.c
+++ open_data_structures/1-introduction/1.1.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int number_one() {
 	char s[] = "hello world!";
 
 	int i;
 	for (i = (sizeof(s) / sizeof(s[0]))-2; i >= 0; i-- ) {
 		printf("%c ", s[i]);
 	}
+	return 0;
+}
+
+int number_two() {
+	char s[] = "Lorem ipsum dolor sit amet, consectetur";
+	int i;
+	int length = sizeof(s) / sizeof(s[0]) - 2;
+	int count;
+	int total;
+
+	if (length >= 50) {
+		total = 50;
+	} else {
+		total = length;
+	}
+
+	for(i = 0; i < total; i++) {
+		printf("%c ", s[length-i]);
+	}
+	printf("\n");
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	number_two();
 }
