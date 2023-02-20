669ee5b532359c6a91e467e86b573b5b98f2b219
kousukekikuchi1984 <kousukekikuchi1984@gmail.com>
2023-02-21 06:20:50 +0900

    remove leet code from book

--- leetcode/src/dynamic_arrays/get_concatenation.rs
+++
@@ -1,8 +0,0 @@
-impl Solution {
-    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
-        let mut nums = nums;
-        let mut ans = nums.clone();
-        ans.append(&mut nums);
-        ans
-    }
-}
